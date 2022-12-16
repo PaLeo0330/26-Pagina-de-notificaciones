8ff5aa952bcbd3855fa585af8d7f959d0548d579
Leonardo Guerra <leonardoguerra1994hhotmail.com>
2022-12-16 14:21:37 -0500

    Se añade numeros en el contenedor de notificaciones leidas y tambien se agrega color a las notificaciones que tengan mayor o igual al numero tres

--- cod.js
+++ cod.js
@@ -1,34 +1,76 @@
-function setNumber () {
+function setNumber() {
     let noLeidas = document.querySelectorAll('.unread');
+    let leidas = document.querySelectorAll('.read')
     let notCounter = document.querySelector('.notificaciones');
-    let yesCounter = document.querySelector('.notificaciones-read');
-    notCounter.innerText = noLeidas.length;
-    yesCounter.innerText = noLeidas.length;
-    
-   
-    if (noLeidas.length === 0){
-        notCounter.classList.remove('zero')
-    } else {
-        notCounter.classList.add('zero')
-    } 
+    let yesCount = document.querySelector('.notificaciones-read');
 
+     notCounter.innerHTML = noLeidas.length;
+     yesCount.innerHTML = leidas.length;
+
+  
+    if (notCounter.innerHTML === '0') {
+        notCounter.classList.add("zero")
+    } else {
+        notCounter.classList.remove("zero")
+    }
+
+    if (notCounter.innerHTML <= '3') {
+        notCounter.classList.add("green")
+    } else  {
+        notCounter.classList.remove("green")
+    }
+
+    if (yesCount.innerHTML === '0') {
+        yesCount.classList.add("one")
+    } else  {
+        yesCount.classList.remove("one")
+    }
+
+
+
+    if (yesCount.innerHTML <= '3') {
+        yesCount.classList.add("red")
+    } else  {
+        yesCount.classList.remove("red")
+    }
+        
+
+    
 }
 
-setNumber()
-let notificaciones = document.querySelectorAll('.notif-card')
-
-notificaciones.forEach(function(desactivar) {
-    desactivar.addEventListener('click', function() {
-        desactivar.classList.toggle('unread');
-        setNumber()
-    })          
-})
+    setNumber()
+    let notificaciones = document.querySelectorAll('.notif-card')
 
 
-let markAll = document.querySelector('.mark-all')
-markAll.addEventListener('click', function() {
-    notificaciones.forEach(function(card) {
-        card.classList.remove('unread');
-        setNumber()
+
+    notificaciones.forEach(function (desactivar) {
+        desactivar.addEventListener('click', function () {
+            desactivar.classList.toggle('unread');
+            setNumber()
+        })
     })
-})
+
+    notificaciones.forEach(function (activar) {
+        activar.addEventListener('click', function () {
+            activar.classList.toggle('read');
+            setNumber()
+        })
+    })
+
+
+
+    let markAll = document.querySelector('.mark-all')
+    markAll.addEventListener('click', function () {
+        notificaciones.forEach(function (card) {
+            card.classList.remove('unread');
+            setNumber()
+        })
+    })
+
+    markAll.addEventListener('click', function () {
+        notificaciones.forEach(function (card) {
+            card.classList.add('read');
+            setNumber()
+        })
+    })
+
